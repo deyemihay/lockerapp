5a0bfc759d2814fc4ad4cd0ca5bfa50f0727e736
deyemihay <62924712+deyemihay@users.noreply.github.com>
2023-01-10 17:11:31 +0200

    changes

--- passwordLocker/settings.py
+++ passwordLocker/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'passwordLocker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fq8cc6FCbolz0HjEva2X',
+        'HOST': 'containers-us-west-100.railway.app',
+        'PORT': '6034',
     }
 }
 
